57e67a188f971b6faf7fa9455e86e032d2a63f8c
Hemal Chevli <hemal.chevli@alislabs.com>
2018-11-22 17:24:01 +0530

    bug fixes for async page loading in suggestions

--- static/js/search.js
+++ static/js/search.js
@@ -3,32 +3,27 @@ var $response = $("#uploadResponse")
 $(".search-button").click(function(e) {
     e.preventDefault();
     var q = $("#search_parameter").val();
-    console.log(q);
       $.ajax({
     type: "GET",
     url: '/search/',
     data: {"q": q},
     success: function(response) {
-    console.log(response);
     $response.empty();
     $response.append(response);
-//      $('#contact_form').html("<div id='message'></div>");
       }
     })
 });
 
-$("#suggest_button").click(function(e) {
+$('.suggestSubmit').click(function(e) {
     e.preventDefault();
-    var q = $("#suggest_button").val();
-    console.log(q);
-      $.ajax({
+    var q = $(this).val();
+    $.ajax({
     type: "GET",
     url: '/suggest/',
     data: {"q": q},
     success: function(response) {
     $response.empty();
     $response.append(response);
-//      $('#contact_form').html("<div id='message'></div>");
       }
     })
 });
